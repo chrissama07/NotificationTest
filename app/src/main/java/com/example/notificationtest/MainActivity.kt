be283566e7c4333b7f610e31f8731f7e2f778154
chris <1553801752@qq.com>
2021-04-25 20:11:17 +0800

    notification test ver3.0

--- app/src/main/java/com/example/notificationtest/MainActivity.kt
+++ app/src/main/java/com/example/notificationtest/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.notificationtest
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -16,15 +18,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
             val channel = NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
             manager.createNotificationChannel(channel)
+
+            val channel2 = NotificationChannel("important", "Important", NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel2)
         }
         sendNotice.setOnClickListener {
-            val notification = NotificationCompat.Builder(this, "normal")
+            val intent = Intent(this, NotificationActivity::class.java)
+            val pi = PendingIntent.getActivity(this, 0, intent, 0)
+            val notification = NotificationCompat.Builder(this, "important")
                 .setContentTitle("This is content title")
                 .setContentText("This is content text")
                 .setSmallIcon(R.drawable.small)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ace))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(resources, R.drawable.ace)))
                 .build()
             manager.notify(1, notification)
         }
